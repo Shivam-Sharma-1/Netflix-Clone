c8e6146bece8f3a41be5a17a3f27b90de4bafca6
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-01 21:36:04 +0530

    feat: add auth page

---
+++ pages/auth.tsx
@@ -0,0 +1,11 @@
+function Auth() {
+    return (
+        <div className=" relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-cover bg-center bg-fixed ">
+            <div className=" bg-black w-full h-full lg:bg-opacity-50 ">
+
+            </div>
+        </div>
+    )
+}
+
+export default Auth 
