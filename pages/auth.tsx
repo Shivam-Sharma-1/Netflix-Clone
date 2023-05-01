64fa964b7949604df01cb3c3ca8795adecf47540
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-01 21:46:15 +0530

    feat: add website logo

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,12 @@
+import Image from "next/image"
+
 function Auth() {
     return (
         <div className=" relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-cover bg-center bg-fixed ">
             <div className=" bg-black w-full h-full lg:bg-opacity-50 ">
-
+                <nav className=" px-12 py-5 ">
+                    <Image src="/images/logo.png" alt="Website-Logo" width={120} height={120} />
+                </nav>
             </div>
         </div>
     )
