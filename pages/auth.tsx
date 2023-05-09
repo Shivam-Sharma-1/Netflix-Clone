06a245499ad0a0b2e54f07e186e54d0cb9490ef3
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-09 16:32:07 +0530

    feat: add google and github signin

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,6 +4,8 @@ import Image from "next/image"
 import { useCallback, useState } from "react"
 import { signIn } from "next-auth/react"
 import { useRouter } from "next/router"
+import { FcGoogle } from "react-icons/fc"
+import { FaGithub } from "react-icons/fa"
 
 function Auth() {
     const router = useRouter()
@@ -83,6 +85,16 @@ function Auth() {
                         <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 duration-500 hover:bg-red-700 hover:duration-500 ">
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
+                        <div className="flex gap-4 items-center justify-center mt-8">
+                            <div className="cursor-pointer w-10 h-10 bg-white rounded-full flex items-center justify-center hover:opacity-80 transition
+                            ">
+                                <FcGoogle size={30}/>
+                            </div>
+                            <div className="cursor-pointer w-10 h-10 bg-white rounded-full flex items-center justify-center hover:opacity-80 transition
+                            ">
+                                <FaGithub size={30}/>
+                            </div>
+                        </div>
                         <p className="text-neutral-500 mt-12">
                             {variant === 'login' ? 'First time using Netflix?' : "Already have an account?"}
                             <span 
