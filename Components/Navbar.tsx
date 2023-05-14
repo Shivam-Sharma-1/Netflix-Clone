a819cead9726488009d4834b0d9fa694cecdf52b
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-14 09:51:05 +0530

    feat: add mobile nav menu

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 import logo from "../public/images/logo.png";
 import NavbarItem from "./NavbarItem";
 import { BsChevronDown } from "react-icons/bs";
+import MobileMenu from "./MobileMenu";
+import { useCallback, useState } from "react";
 
 function Navbar() {
+	const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+	const toggleMobileMenu = useCallback(() => {
+		setShowMobileMenu((current) => !current);
+	}, []);
+
 	return (
 		<nav className="w-full fixed z-40">
 			<div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
@@ -22,9 +30,13 @@ function Navbar() {
 					<NavbarItem label="My List" />
 					<NavbarItem label="Browse by languages" />
 				</div>
-				<div className="flex flex-row gap-2 items-center ml-8 cursor-pointer relative lg:hidden">
+				<div
+					className="flex flex-row gap-2 items-center ml-8 cursor-pointer relative lg:hidden"
+					onClick={toggleMobileMenu}
+				>
 					<p className="text-white text-sm">Browse</p>
 					<BsChevronDown className="text-white transition" />
+					<MobileMenu visible={showMobileMenu} />
 				</div>
 			</div>
 		</nav>
