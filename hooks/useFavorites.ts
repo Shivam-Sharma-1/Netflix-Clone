6d406f9ec69330cd40286797146387a0e95d55af
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-18 12:40:10 +0530

    feat: add useFavorite hook

---
+++ hooks/useFavorites.ts
@@ -0,0 +1,14 @@
+import fetcher from "@/lib/fetcher";
+import useSWR from "swr";
+
+function useFavorites() {
+	const { data, error, isLoading, mutate } = useSWR(
+		"/api/favourites",
+		fetcher,
+		{
+			revalidateIfStale: false,
+			revalidateOnFocus: false,
+			revalidateOnReconnect: false,
+		}
+	);
+}
