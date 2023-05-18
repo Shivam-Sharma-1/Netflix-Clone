7371c394bc26088e1fdf91b93db519f6c935f618
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-18 15:13:56 +0530

    fix: movie card button

--- Components/MovieCard.tsx
+++ Components/MovieCard.tsx
@@ -78,9 +78,9 @@ const MovieCard: FC<MovieCardProps> = ({ data }) => {
 								className="text-black w-4 lg:w-6"
 								size={30}
 							/>
-							<div>
-								<FavoriteButton movieId={data?.id} />
-							</div>
+						</div>
+						<div>
+							<FavoriteButton movieId={data?.id} />
 						</div>
 
 						<p className="text-green-400 font-semibold mt-4">
