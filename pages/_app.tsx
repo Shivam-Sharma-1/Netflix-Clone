93d7a78e03307909c880850cc0340abeea608eb8
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-20 20:04:39 +0530

    feat: add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({
 	Component,
@@ -8,6 +9,27 @@ export default function App({
 }: AppProps) {
 	return (
 		<SessionProvider session={session}>
+			<Head>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/images/favicon/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/images/favicon/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/images/favicon/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+				<title>Netflix</title>
+			</Head>
 			<Component {...pageProps} />
 		</SessionProvider>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,11 @@ import Billboard from "@/Components/Billboard";
 import InfoModal from "@/Components/InfoModal";
 import MovieList from "@/Components/MovieList";
 import Navbar from "@/Components/Navbar";
-import useCurrentUser from "@/hooks/useCurrentUser";
 import useFavourites from "@/hooks/useFavourites";
 import useInfoModal from "@/hooks/useInfoModal";
 import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
 	const session = await getSession(context);
