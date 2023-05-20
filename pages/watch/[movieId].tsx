67a5f822da12c6e3712c40c580b592ea3dd36782
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-20 18:14:48 +0530

    add cursor pointer to arrow

--- pages/watch/[movieId].tsx
+++ pages/watch/[movieId].tsx
@@ -12,7 +12,7 @@ function Watch() {
 			<nav className="fixed w-full p-4 z-10 flex flex-row items-center gap-8 bg-black bg-opacity-70">
 				<AiOutlineArrowLeft
 					onClick={() => router.back()}
-					className="text-white"
+					className="text-white cursor-pointer"
 					size={40}
 				/>
 				<p className="text-white text-1xl md:text-3xl font-bold">
